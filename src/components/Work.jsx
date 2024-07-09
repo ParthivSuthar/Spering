a25d5145ac0bbf06926802176079c4e451caeaa7
Parthiv Suthar <myworkflow7070@gmail.com>
2024-07-09 13:44:02 +0530

    add Work Section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,31 @@
-
+import { workDetails } from "../constants/data"
+import freelanceimg from "../assets/freelance-img.jpg"
 
 function Work() {
   return (
-    <div>Work</div>
+    <div className="w-full">
+      <div className="grid grid-cols-2 place-items-center h-[500px]">
+        <div>
+          <h2 className="text-2xl font-extrabold">WORK FREELANER HAS DONE</h2>
+          <div>
+            <ul>
+              {workDetails.map((dets, index) => (
+                <div className="flex gap-3 pt-5" key={index}>
+                  <img src={dets.image} alt="img" className="bg-slate-800 w-12 rounded-full p-3" />
+                  <div>
+                    <span className="text-red-600 font-bold">{dets.num}</span>
+                    <h3 className="text-xl font-bold">{dets.desc}</h3>
+                  </div>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div>
+          <img src={freelanceimg} alt="freelanceimg" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/constants/data.js
+++ src/constants/data.js
@@ -6,6 +6,10 @@ import linkedin from "../assets/linkedin.png"
 import location from "../assets/location.png"
 import mail from "../assets/mail.png"
 import phone from "../assets/call.png" 
+import cash from "../assets/f1.png"
+import invoice from "../assets/f2.png"
+import freelancer from "../assets/f3.png"
+import customer from "../assets/f4.png"
 
 
 
@@ -29,4 +33,11 @@ export const contactDetails = [
     {detail: "Location", image: location},
     {detail: "demo@gmail.com", image: mail},
     {detail: "Call +01 1234567890", image: phone}
+]
+
+export const workDetails = [
+    {image: cash, num: "$250 Million", desc: "Paid to Freelancers"},
+    {image: invoice, num: "2 Million", desc: "Paid Invoices"},
+    {image: freelancer, num: "700,000", desc: "Worldwide Freelancer"},
+    {image: customer, num: "98%", desc: "Customer Satisfaction Rate"},
 ]
